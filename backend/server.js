b7b57bd9a1f0b21ad0a612d813b221be4fa1b666
Shatakshi Goel <goelshatakshi19@gmail.com>
2025-03-31 01:52:02 +0530

    feat: enhance server configuration for production and add build script

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-//module is added in type so that we can directly use import function for importing libraries
-
+import path from "path";
 import express from "express";
-import dotenv from "dotenv"; //A library to load environment variables from a .env file into process.env
-import cookieParser from "cookie-parser"; //A library for parsing cookies in HTTP requests
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //routes
 import authRoutes from "./routes/auth.route.js";
@@ -18,11 +17,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const__dirname = path.resolve();
 //console.log(process.env.PORT);
 
 // dev is used for development when server will be starting again and again on introducing some changes while start is used in production when server will not start again
 
-app.use(express.json({limit:"10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -32,15 +33,18 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   // listen is used to start the server
   console.log("Server is running on http://localhost:" + PORT);
 
   connectDB();
   console.log("Database connected successfully");
-});
-
-// Add error handling for server startup
-app.on('error', (err) => {
-  console.error('Server error:', err);
 });
